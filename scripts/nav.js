1942e1f390ae75b23aed84251f0747e73a8fbd39
chiwiwiche <chiwiwiche@gmail.com>
2024-04-26 15:35:55 -0600

    changes

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,7 +1,24 @@
-function openNav(){
-    document.getElementById("mobile-menu").style.width = "100%";
-}
+// function openNav(){
+//     document.getElementById("mobile-menu").style.width = "100%";
+// }
 
-function closeNav(){
-    document.getElementById("mobile-menu").style.width = "0%";
-}
+// function closeNav(){
+//     document.getElementById("mobile-menu").style.width = "0%";
+// }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.querySelector('.menu');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuButton.addEventListener('click', function () {
+        mobileMenu.style.width = "100%";
+    });
+
+    const closeButtons = document.querySelectorAll('.close, .overlay-content a');
+    closeButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            mobileMenu.style.width = "0%";
+        });
+    });
+});
